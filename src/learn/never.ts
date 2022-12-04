984554d9e72448cf0720faee6e30e2a350f554d9
alawnxu <253948113@qq.com>
2022-12-04 16:26:28 +0800

    feat: awaited

---
+++ src/learn/never.ts
@@ -0,0 +1,6 @@
+function throwMyException() {
+  throw new Error('MyException');
+}
+
+throwMyException();
+console.log('hello world'); // not exception
---
+++ src/type-challenges/awaited/index.ts
@@ -0,0 +1,22 @@
+type ExampleType = Promise<string>;
+
+type MyAwaited<T> = T extends Promise<infer Type> ? Type : never;
+
+type Result2 = MyAwaited<ExampleType>;
+type Result3 = MyAwaited<string>;
+
+// => Promise<number>
+type Result4 = MyAwaited<Promise<Promise<number>>>;
+
+// 递归(如果要取出递归后的类型数据)
+type FullMyAwaited<T> = T extends Promise<infer Type>
+  ? Type extends Promise<infer InnerType>
+    ? FullMyAwaited<Type>
+    : Type
+  : never;
+
+type Result5 = FullMyAwaited<string>;
+type Result6 = FullMyAwaited<Promise<number>>;
+
+// => number
+type Result7 = FullMyAwaited<Promise<Promise<number>>>;
