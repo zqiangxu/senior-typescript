2a08eefd44be3094bf4a3c723cc8c4d35b06fd48
alawnxu <253948113@qq.com>
2022-12-04 13:09:15 +0800

    feat: best

--- src/type-challenges/first-of-array/index.ts
+++ src/type-challenges/first-of-array/index.ts
@@ -5,3 +5,16 @@ type First<T extends readonly any[]> = T[0];
 
 type head1 = First<arr1>; // expected to be 'a'
 type head2 = First<arr2>;
+
+// 空数组 => T[0] => undefined
+
+type arr3 = [];
+type head3 = First<arr3>; // => undefined.
+
+// 如果需要存在的时候返回 never
+
+type BestFirst<T extends any[]> = T extends [infer head, ...any[]] ? head : never;
+
+// => never
+type head4 = BestFirst<arr3>;
+type head5 = BestFirst<arr1>;
