5282010d49093749495b5826736e21122843fe96
alawnxu <253948113@qq.com>
2022-12-04 13:00:41 +0800

    feat: tuple length

---
+++ src/type-challenges/tuple-length/index.ts
@@ -0,0 +1,7 @@
+type tesla = ['tesla', 'model 3', 'model X', 'model Y'];
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'];
+
+type Length<T extends readonly any[]> = T['length'];
+
+type teslaLength = Length<tesla>; // expected 4
+type spaceXLength = Length<spaceX>; // expected 5
