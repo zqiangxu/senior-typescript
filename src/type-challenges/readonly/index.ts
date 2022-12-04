44d64847b25f9a072f1e61385cb6b5c18d6e237a
alawnxu <253948113@qq.com>
2022-12-04 10:51:07 +0800

    feat: readonly

---
+++ src/type-challenges/readonly/index.ts
@@ -0,0 +1,16 @@
+interface Todo2 {
+  title: string;
+  description: string;
+}
+
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K];
+};
+
+const todo: MyReadonly<Todo2> = {
+  title: 'Hey',
+  description: 'foobar',
+};
+
+// readonly
+// todo.title = '12';
