07cf1311ba35b39687acba12bf643da1451d7855
alawnxu <253948113@qq.com>
2022-12-08 09:38:26 +0800

    feat: trim

--- src/type-challenges/trim/index.ts
+++ src/type-challenges/trim/index.ts
@@ -8,4 +8,20 @@ type Result1 = Trim<'  Hello World  '>;
 type Result2 = Trim<'      Hello World      '>;
 type Result3 = Trim<'Hello       '>;
 type Result4 = Trim<' '>;
+
+// Best
+type BestWhiteSpace = ' ' | '\n' | '\t';
+type BestTrim<T extends string> = T extends `${BestWhiteSpace}${infer D}` | `${infer D}${BestWhiteSpace}`
+  ? BestTrim<D>
+  : T;
+
+type Result5 = BestTrim<'  Hello World  '>;
+type Result6 = BestTrim<'      Hello World      '>;
+type Result7 = BestTrim<'Hello       '>;
+type Result8 = BestTrim<' '>;
+type Result9 = BestTrim<`
+Hello World
+
+`>;
+
 export { Trim };
