802e2f837b971ced9ad0f625c078990166b8dadf
alawnxu <253948113@qq.com>
2022-12-11 22:17:27 +0800

    feat: diff

--- src/type-challenges/00645-medium-diff/index.ts
+++ src/type-challenges/00645-medium-diff/index.ts
@@ -16,11 +16,20 @@ type Diff<T, U> = {
   [K in keyof Omit<U, keyof T> | keyof Omit<T, keyof U>]: K extends keyof T ? T[K] : K extends keyof U ? U[K] : never;
 };
 
-type a = Omit<Foo, 'name'>;
-
 type Result1 = Diff<Foo, Bar>;
 type Result2 = Diff<Bar, Foo>;
 type Result3 = Diff<Foo, Coo>;
 type Result4 = Diff<Coo, Foo>;
+
+// 合并 U 和 T，并排除公共的属性的属性
+type BestDiff<T, U> = Omit<U & T, keyof T & keyof U>;
+
+// => B
+type Key = ('A' | 'B') & 'B';
+
+type Result5 = Diff<Foo, Bar>;
+type Result6 = Diff<Bar, Foo>;
+type Result7 = Diff<Foo, Coo>;
+type Result8 = Diff<Coo, Foo>;
 
 export { Diff };
