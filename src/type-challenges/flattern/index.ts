2df621eadbabc0cbb82231193512adeb03aa09a3
alawnxu <253948113@qq.com>
2022-12-11 19:18:05 +0800

    feat: flattern

---
+++ src/type-challenges/flattern/index.ts
@@ -0,0 +1,9 @@
+type IsArray<T> = T extends any[] ? true : false;
+
+type Flatten<T extends any[]> = T extends [infer D, ...infer F]
+  ? D extends any[]
+    ? [...Flatten<D>, ...Flatten<F>]
+    : [D, ...Flatten<F>]
+  : [];
+
+type flatten = Flatten<[1, 2, [3, 4], [[[5]]], [6, 7], [8]]>;
