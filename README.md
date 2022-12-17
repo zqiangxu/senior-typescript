9fdc5eab6a14df29b06715fb9fcd19cd92954dc8
alawnxu <253948113@qq.com>
2022-12-17 22:04:16 +0800

    feat: partialByKeys

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # senior-typescript
-高级TypeScript学习. 其中 type-challenges 来源于: https://github.com/type-challenges/type-challenges
+
+高级 TypeScript 学习，同时会收集一起比较常用的 TS 体操。
+
+其中 type-challenges 来源于: https://github.com/type-challenges/type-challenges
---
+++ src/type-challenges/02757-medium-partialbykeys/index.ts
@@ -0,0 +1,36 @@
+interface User {
+  name: string;
+  age: number;
+  address: string;
+}
+
+interface UserPartialName {
+  name?: string;
+  age: number;
+  address: string;
+}
+
+interface UserPartialNameAndAge {
+  name?: string;
+  age?: number;
+  address: string;
+}
+
+// 会自动取|
+type PickValidKey<T, U> = U extends keyof T ? U : never;
+
+type PartialByKeys<T, U extends keyof T = keyof T> = Omit<T, U> & {
+  // [key in PickValidKey<T, U>]?: key extends keyof T ? T[key] : never;
+  [key in U extends keyof T ? U : never]?: key extends keyof T ? T[key] : never;
+};
+
+type r1 = PartialByKeys<User, 'name'>;
+type r2 = PartialByKeys<User, 'name' | 'age'>;
+type r3 = PartialByKeys<User>;
+
+// @ts-expect-error
+type r4 = PartialByKeys<User, 'name' | 'unknown'>;
+
+const a: r1 = { age: 12, address: 'address' };
+const b: r3 = {};
+const c: r4 = { age: 12, address: 'address' };
