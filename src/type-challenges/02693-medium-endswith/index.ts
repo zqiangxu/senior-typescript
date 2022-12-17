fbd91e383b4848c73b92e57eb34308c79bbe6a03
alawnxu <253948113@qq.com>
2022-12-17 21:42:41 +0800

    feat: ends with

--- src/type-challenges/02693-medium-endswith/index.ts
+++ src/type-challenges/02693-medium-endswith/index.ts
@@ -10,3 +10,13 @@ type a = EndsWith<'abc', 'bc'>; // expected to be true
 type b = EndsWith<'abc', 'abc'>; // expected to be true
 type c = EndsWith<'abc', 'd'>; // expected to be false
 type d = EndsWith<'', ''>; // expected to be false
+
+// 使用 ${string} 代替任意的字符串。TS 支持这种后缀匹配. 参考: 02688-medium-startswith
+type BestEndsWith<T extends string, U extends string> = T extends `${string}${U}` ? true : false;
+
+type a1 = BestEndsWith<'abc', 'bc'>; // expected to be true
+type b1 = BestEndsWith<'abc', 'abc'>; // expected to be true
+type c1 = BestEndsWith<'abc', 'd'>; // expected to be false
+type d1 = BestEndsWith<'', ''>; // expected to be false
+
+export { EndsWith, BestEndsWith };
