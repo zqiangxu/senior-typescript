239483de874be9872e25dbafcca044bb2e4e15ca
alawnxu <253948113@qq.com>
2022-12-17 21:34:17 +0800

    feat: BestStartWith

--- src/type-challenges/02688-medium-startswith/index.ts
+++ src/type-challenges/02688-medium-startswith/index.ts
@@ -8,4 +8,12 @@ type a = StartsWith<'abc', 'ac'>; // expected to be false
 type b = StartsWith<'abc', 'ab'>; // expected to be true
 type c = StartsWith<'abc', 'abcd'>; // expected to be false
 
+// best
+// 神奇的写法. 使用 ${string} 来代表任意的字符串
+type BestStartsWith<T extends string, U extends string> = T extends `${U}${string}` ? true : false;
+
+type a1 = BestStartsWith<'abc', 'ac'>; // expected to be false
+type b1 = BestStartsWith<'abc', 'ab'>; // expected to be true
+type c1 = BestStartsWith<'abc', 'abcd'>; // expected to be false
+
 export { StartsWith };
