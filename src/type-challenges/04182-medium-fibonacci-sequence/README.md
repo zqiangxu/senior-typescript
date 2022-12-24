54d134f97140edcf88f18f679f4a45f238362db7
alawnxu <253948113@qq.com>
2022-12-24 15:30:27 +0800

    feat: 04182-medium-fibonacci-sequence

---
+++ src/type-challenges/04182-medium-fibonacci-sequence/README.md
@@ -0,0 +1,10 @@
+Implement a generic Fibonacci<T> that takes a number T and returns its corresponding Fibonacci number.
+
+The sequence starts: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+
+For example
+
+```ts
+type Result1 = Fibonacci<3>; // 2
+type Result2 = Fibonacci<8>; // 21
+```
---
+++ src/type-challenges/04182-medium-fibonacci-sequence/index.ts
@@ -0,0 +1,30 @@
+// 向数组中填满指定长度的0元素
+type FillRepeatZeroArray<N extends number, Arr extends any[] = []> = Arr['length'] extends N
+  ? Arr
+  : FillRepeatZeroArray<N, [0, ...Arr]>;
+
+type A1 = FillRepeatZeroArray<1>; // [0]
+type A2 = FillRepeatZeroArray<2>; // [0,0]
+
+type Fibonacci<
+  N extends number,
+  Fibs extends number[] = [1],
+  MinusOneFibs extends number[] = FillRepeatZeroArray<0>,
+  MinusTwoFibs extends number[] = FillRepeatZeroArray<1>
+> = N extends 1
+  ? 1
+  : Fibs['length'] extends N
+  ? // 通过合并数组实现加法
+    [...MinusOneFibs, ...MinusTwoFibs]['length']
+  : // [1, ...Fibs] 用于索引 + 1
+    // 更新两个临时的 Fibs . 通过合并数组的长度来表示加法
+    Fibonacci<N, [1, ...Fibs], [...MinusOneFibs, ...MinusTwoFibs], MinusOneFibs>;
+
+type Result1 = Fibonacci<3>; // 2
+type Result2 = Fibonacci<8>; // 21
+type Result3 = Fibonacci<4>; // 3
+type Result4 = Fibonacci<2>; // 1
+type Result5 = Fibonacci<7>; // 13
+type Result6 = Fibonacci<9>; // 34
+
+export { Fibonacci };
