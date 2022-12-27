4d0a84c0dedec1bc9d718295567ae611b21676a5
alawnxu <253948113@qq.com>
2022-12-27 22:07:01 +0800

    feat: 08987-medium-subsequence

---
+++ type-challenges/08987-medium-subsequence/index.ts
@@ -0,0 +1,10 @@
+// 比较简单的一题，没有要求出所有组合
+type Subsequence<T extends readonly any[]> = T extends [infer First, ...infer Rest]
+  ? // 递归即可
+    [First] | [First, ...Subsequence<Rest>] | Subsequence<Rest>
+  : [];
+
+type A = Subsequence<[1, 2]>; // [] | [1] | [2] | [1, 2]
+type B = Subsequence<[1, 2, 3]>; // [] | [1] | [2] | [1, 2] | [3] | [1, 3] | [2, 3] | [1, 2, 3]
+
+export { Subsequence };
