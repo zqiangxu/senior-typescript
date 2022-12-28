5697ac3989a3975978c35a9b574032a2c965d47e
alawnxu <253948113@qq.com>
2022-12-28 22:03:29 +0800

    feat: BestGetMiddleElement

--- type-challenges/09896-medium-get-middle-element/index.ts
+++ type-challenges/09896-medium-get-middle-element/index.ts
@@ -21,3 +21,24 @@ type simple5 = GetMiddleElement<[1, 2]>; // expected to be [1, 2]
 type simple6 = GetMiddleElement<[1]>; // expected to be [1]
 type simple7 = GetMiddleElement<[never]>;
 type simple8 = GetMiddleElement<[() => string, () => number]>;
+
+/**
+ * @see {@link https://github.com/type-challenges/type-challenges/issues/18774}
+ * 还能这样玩
+ */
+type BestGetMiddleElement<T extends readonly unknown[]> = T['length'] extends 0 | 1 | 2
+  ? T
+  : T extends [unknown, ...infer Middle, unknown]
+  ? GetMiddleElement<Middle>
+  : never;
+
+type bSimple1 = GetMiddleElement<[1, 2, 3, 4, 5]>; // expected to be [3]
+type bSimple2 = GetMiddleElement<[1, 2, 3, 4, 5, 6]>; // expected to be [3, 4]
+type bSimple3 = GetMiddleElement<[1, 2, 3, 4]>; // expected to be [2, 3]
+type bSimple4 = GetMiddleElement<[1, 2, 3]>; // expected to be [2]
+type bSimple5 = GetMiddleElement<[1, 2]>; // expected to be [1, 2]
+type bSimple6 = GetMiddleElement<[1]>; // expected to be [1]
+type bSimple7 = GetMiddleElement<[never]>;
+type bSimple8 = GetMiddleElement<[() => string, () => number]>;
+
+export { GetMiddleElement, BestGetMiddleElement };
