ca06205005e37c2136f126f218913f0b9f549de1
alawnxu <253948113@qq.com>
2022-12-29 22:40:39 +0800

    fix: array iterator

--- type-challenges/16259-medium-to-primitive/index.ts
+++ type-challenges/16259-medium-to-primitive/index.ts
@@ -1,11 +1,25 @@
 // 判断是否为一个 object
 type IsObject<T> = T extends Record<string | number | symbol, unknown> ? true : false;
 
+// 判断是否为一个数组
+type IsArray<T> = T extends any[] ? true : false;
+
+type IA1 = IsArray<[]>;
+type IA2 = IsArray<[1, 2]>;
+type IA3 = IsArray<1>;
+type ToArrayPrimitiveType<T extends any[]> = T extends [infer First, ...infer Rest]
+  ? [ToPrimitiveType<First>, ...ToArrayPrimitiveType<Rest>]
+  : [];
+
 // 转换为原始类型
 type ToPrimitiveType<T> = T extends number ? number : T extends boolean ? boolean : T extends string ? string : unknown;
 
 type ToPrimitive<T> = {
-  [Key in keyof T]: IsObject<T[Key]> extends true ? ToPrimitive<T[Key]> : ToPrimitiveType<T[Key]>;
+  [Key in keyof T]: IsObject<T[Key]> extends true
+    ? ToPrimitive<T[Key]>
+    : T[Key] extends any[]
+    ? ToArrayPrimitiveType<T[Key]>
+    : ToPrimitiveType<T[Key]>;
 };
 
 type X = {
@@ -16,16 +30,8 @@ type X = {
     home: '123456';
     phone: '13111111111';
   };
+  hobbies: ['sing', 'dance'];
 };
 
-type Expected = {
-  name: string;
-  age: number;
-  married: boolean;
-  addr: {
-    home: string;
-    phone: string;
-  };
-};
 type Todo = ToPrimitive<X>; // should be same as `Expected`
 type a = Todo['addr'];
