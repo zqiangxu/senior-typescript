4c23974b8ec5f992490698a18ffeb3fc0c08ea92
alawnxu <253948113@qq.com>
2023-01-14 17:59:28 +0800

    feat: 14080-hard-fizzbuzz

---
+++ type-challenges/14080-hard-fizzbuzz/index.ts
@@ -0,0 +1,32 @@
+type Mod<
+  T extends unknown[],
+  N extends number,
+  A extends unknown[] = [1],
+  B extends unknown[] = [1]
+> = B['length'] extends T['length']
+  ? A['length'] extends N
+    ? true
+    : false
+  : A['length'] extends N
+  ? Mod<T, N, [1], [1, ...B]>
+  : Mod<T, N, [1, ...A], [1, ...B]>;
+
+type M1 = Mod<[1, 1, 1], 3>;
+type M2 = Mod<[1, 1, 1, 1, 1], 5>;
+type M3 = Mod<[1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 5>;
+
+type Output<T extends unknown[]> = Mod<T, 3> extends true
+  ? Mod<T, 5> extends true
+    ? 'FizzBuzz'
+    : 'Fizz'
+  : Mod<T, 5> extends true
+  ? 'Buzz'
+  : T['length'];
+
+type FizzFuzz<T extends number, Arr extends unknown[] = [1]> = Arr['length'] extends T
+  ? Output<Arr>
+  : Output<Arr> | FizzFuzz<T, [...Arr, 1]>;
+
+type F1 = FizzFuzz<23>;
+
+export { FizzFuzz };
