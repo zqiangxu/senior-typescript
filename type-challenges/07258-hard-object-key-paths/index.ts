f63b4ae1f554a57860b1cc59242f2feb01af5748
alawnxu <253948113@qq.com>
2023-01-14 15:16:33 +0800

    feat: 兼容数组的写法完善

--- type-challenges/07258-hard-object-key-paths/index.ts
+++ type-challenges/07258-hard-object-key-paths/index.ts
@@ -5,7 +5,7 @@ type ObjectKeyPaths<T, PropertyBasePath extends string = '', Key extends keyof T
   ?
       | `${PropertyBasePath}${Key}`
       // 兼容数组写法
-      | (Key extends number ? `${PropertyBasePath}[${Key}]` : never)
+      | (T extends any[] ? `${PropertyBasePath}[${Key}]` : never)
       // 如果是一个 对象. 数组也是继承 object
       | (T[Key] extends object
           ? // 加上根节点和自己的属性
@@ -16,11 +16,13 @@ type ObjectKeyPaths<T, PropertyBasePath extends string = '', Key extends keyof T
 type Book = { name: string; price: number };
 
 type T1 = ObjectKeyPaths<{ name: string; age: number }>; // expected to be 'name' | 'age'
+
 type T2 = ObjectKeyPaths<{
   refCount: number;
   person: { name: string; age: number };
 }>; // expected to be 'refCount' | 'person' | 'person.name' | 'person.age'
 type T3 = ObjectKeyPaths<{ books: [{ name: string; price: number }] }>; // expected to be the superset of 'books' | 'books.0' | 'books[0]' | 'books.[0]' | 'books.0.name' | 'books.0.price' | 'books.length' | 'books.find'
+type T4 = ObjectKeyPaths<{ 1: number; fox: number }>;
 
 type IsObj = Book extends object ? true : false;
 type IsObj2 = [Book] extends object ? true : false;
