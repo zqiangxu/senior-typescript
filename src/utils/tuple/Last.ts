805056d5ba93b92c729c6676d874cb0d8ca8a916
alawnxu <253948113@qq.com>
2023-01-25 11:22:34 +0800

    feat: 修复 Last 的BUG

--- src/utils/tuple/Last.ts
+++ src/utils/tuple/Last.ts
@@ -1,7 +1,8 @@
 /**
  * 获取数组最后一个类型
+ * @version 1.0.0
  */
-type Last<T extends any[]> = T extends [...any[], infer D] ? D : never;
+type Last<T extends readonly unknown[]> = T extends readonly [...unknown[], infer D] ? D : never;
 
 type R1 = Last<['a', 'b', 'c']>; // 'c'
 type R2 = Last<[3, 2, 1]>; // 1
@@ -9,5 +10,11 @@ type R3 = Last<[string, 2, number]>; // number
 type R4 = Last<[any, unknown]>; // unknown
 type R5 = Last<[any, never]>; // never
 type R6 = Last<[never]>; // never
+
+const array = [1,2,3]
+type R7 = Last<typeof array>; // never
+
+const readonlyArray = [1,2,3] as const;
+type R8 = Last<typeof readonlyArray>; // 3
 
 export { Last };
