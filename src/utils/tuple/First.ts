0a2ee52854d945884170c922f7ca4fdf2478aeed
alawnxu <253948113@qq.com>
2023-01-25 11:23:57 +0800

    fix: 修复 First 的BUG

--- src/utils/tuple/First.ts
+++ src/utils/tuple/First.ts
@@ -1,9 +1,15 @@
 /**
  * 获取数组第一个类型
  */
-type First<T extends any[]> = T extends [infer head, ...any[]] ? head : never;
+type First<T extends readonly any[]> = T extends readonly [infer head, ...any[]] ? head : never;
 
 type R1 = First<['a', 'b', 'c']>; // a
 type R2 = First<[3, 2, 1]>; // 3
+
+const array = [1,2,3]
+type R7 = First<typeof array>; // never
+
+const readonlyArray = [1,2,3] as const;
+type R8 = First<typeof readonlyArray>; // 1
 
 export { First };
