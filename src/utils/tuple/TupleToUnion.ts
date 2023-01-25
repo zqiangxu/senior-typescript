fd78c7e88c4b5e35b70f7619afdeac509db145dd
alawnxu <253948113@qq.com>
2023-01-25 11:17:31 +0800

    feat: 更新 TupleToUnion

--- src/utils/tuple/TupleToUnion.ts
+++ src/utils/tuple/TupleToUnion.ts
@@ -1,4 +1,4 @@
-type TupleToUnion<T extends any[]> = T[number];
+type TupleToUnion<T> = T extends readonly unknown[] ? T[number] : never;
 
 type Result1 = TupleToUnion<[123, '456', true]>;
 type Result2 = TupleToUnion<[123]>;
