ca94a5839c4456e14dbc113d55425510d99caddd
alawnxu <253948113@qq.com>
2023-01-27 18:16:47 +0800

    feat: shift

--- src/utils/tuple/Shift.ts
+++ src/utils/tuple/Shift.ts
@@ -1,12 +1,12 @@
 type Shift<T extends readonly unknown[]> = T extends readonly [unknown, ...infer Rest] ? Rest : never;
 
-type R1 = Shift<['a', 'b', 'c', 'd']>;
-type R2 = Shift<[3, 2, 1]>;
+type R1 = Shift<['a', 'b', 'c', 'd']>; // ["b", "c", "d"]
+type R2 = Shift<[3, 2, 1]>; // [2, 1]
 
 const array = ["hello", "world"];
 type R3 = Shift<typeof array>; // never
 
 const readonlyArray = ["hello", "world"] as const;
-type R4 = Shift<typeof readonlyArray>; // never
+type R4 = Shift<typeof readonlyArray>; // ["world"]
 
 export { Shift }
